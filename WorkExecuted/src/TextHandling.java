9b0988a262a1be0ecc2e6389faf804eb710999c0
Denis Rychagov <gh2rda@mail.ru>
2019-01-14 18:00:14 +0300

    start point
    Исходное задание

---
+++ WorkExecuted/src/TextHandling.java
@@ -0,0 +1,13 @@
+package ru.sbt.school.collection.rda;
+import java.lang.StringBuilder;
+import java.util.Set;
+public class TextHandling {
+
+    public static void main(String[] args) {
+    StringBuilder text=new StringBuilder("");
+    text.append("dlsmkf;lsdma а sdfasdf").append("sdfksalkflsdkfh да qew йцоуд ");
+
+    }
+
+
+}
